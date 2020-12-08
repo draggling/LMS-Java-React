076d45805c00d302a788c055d2f5c9d210f72bc6
draggling <45193915+draggling@users.noreply.github.com>
2020-12-08 17:44:17 -0500

    Update Spinner.js

--- src/components/Util/Spinner.js
+++ src/components/Util/Spinner.js
@@ -1,30 +1,19 @@
 'use strict';
-
 import React from 'react';
-import RingLoader from "react-spinners/RingLoader";
-
+import RingLoader from 'react-spinners/RingLoader';
 const Spinner = (...key) => {
-	let content = "";
-	(key && typeof (key[0] === 'number' || key[0] === 'string'))
-	? content = (
-		<div className="d-flex justify-content-center" key = {key}>
-			<RingLoader
-			size={150}
-			color={"#00ccff"}
-			loading={true}
-			/>)
-		</div> 
-	)
-	: content = (
-		<div className="d-flex justify-content-center">
-		<RingLoader
-		size={150}
-		color={"#00ccff"}
-		loading={true}
-		/>)
-	</div> 
-	);
+	let content = '';
+	key && typeof (key[0] === 'number' || key[0] === 'string')
+		? (content = (
+				<div className="d-flex justify-content-center" key={key}>
+					<RingLoader size={150} color={'#00ccff'} loading={true} />
+				</div>
+		  ))
+		: (content = (
+				<div className="d-flex justify-content-center">
+					<RingLoader size={150} color={'#00ccff'} loading={true} />
+				</div>
+		  ));
 	return content;
 };
-
 export default Spinner;
